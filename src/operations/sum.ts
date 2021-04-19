b3943f6b037b5f52c219aa7e784d66c0acd3bce4
Francisco Avila <frank@hover.to>
2021-04-18 19:25:19 -0700

    docs(operations): add description for operation methods

--- src/operations/sum.ts
+++ src/operations/sum.ts
@@ -1,3 +1,10 @@
+/**
+ *
+ * This method computes the sum of two numbers
+ * @param {number} a
+ * @param {number} b
+ * @returns {number} returns the result of a + b
+ */
 function sum(a: number, b: number): number {
   return a + b;
 }
