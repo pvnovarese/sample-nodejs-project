96b84bfd938294cbf6fd19e3910f1975e62adc1c
Francisco Avila <frank@hover.to>
2021-04-18 17:58:55 -0700

    feat(example): add example to run compiled TS lib as CommonJS module

---
+++ examples/hello-world.cjs.js
@@ -0,0 +1,7 @@
+const { sum, subtract } = require('../lib/cjs/operations');
+
+const a = 4;
+const b = 3;
+
+console.log(`${a}+${b}=${sum(a, b)}`);
+console.log(`${a}-${b}=${subtract(a, b)}`);
---
+++ lib/esm/operations/subtract.d.ts
@@ -0,0 +1,2 @@
+declare function subtract(a: number, b: number): number;
+export { subtract as default };
